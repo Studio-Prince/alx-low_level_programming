d2f861fbdf15c45517446de54db59b8db41d85e2
‘Studio-Prince’ <mosesice5@gmail.com>
2022-12-08 06:51:59 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,22 +1,21 @@
-#include <stdio.h>
-
-
+#include<stdio.h>
 
 /**
  *
- *  * main - Prints quote
+ *  * main - Entry point
  *
  *   *
  *
- *    * Return: zero on success
+ *    * Return: Always 0 (Success)
  *
- */
+ *     */
 
 int main(void)
 
 {
 
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+		puts("\"Programming is like building a multilingual puzzle");
+
+		return (0);
 
 }
