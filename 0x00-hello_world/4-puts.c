5a0a2e4e17fc155a507287d66026403ac7501b4e
‘Studio-Prince’ <mosesice5@gmail.com>
2022-12-08 06:46:25 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 
+
+
 /**
  *
- *  * main - Entry point
+ *  * main - Prints quote
  *
  *   *
  *
- *    * Return: Always 0 (Success)
+ *    * Return: zero on success
  *
- *     */
+ */
 
 int main(void)
 
 {
-	
+
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 
