66da44f178c0e627532e1c48505472ca7f623a89
‘Studio-Prince’ <mosesice5@gmail.com>
2022-12-09 06:51:06 -0800

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - The last digit
+ * @void: Empty parameter list for main.
+ *
+ * Description: Prints last digit of random number and tells
+ * if it is greater than 5, 0, or less than 6 and not 0.
+ *
+ * Return: 0 for success
+*/
+int main(void)
+{
+	char moses;
+
+
+	for (moses = 'a'; moses < 'z'; moses++);
+	{
+		putchar(moses);
+	}
+	putchar("\n");
+	return (0);
+}
