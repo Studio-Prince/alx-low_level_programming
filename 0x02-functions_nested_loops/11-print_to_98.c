c1a9ea3a43a8f24e29ea5824d03250123171552d
‘Studio-Prince’ <mosesice5@gmail.com>
2022-12-13 08:25:53 -0800

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,32 +7,21 @@
 void print_to_98(int n)
 {
 
-	if (n >= 98)
 
+	int i;
+
+	if (n <= 98)
 	{
-
-		while (n > 98)
-
-			printf("%d, ", n--);
-
-		printf("%d\n", n);
-
-	}
-
-
-
-	else
-
+		for (i = n; i < 98; i++)
+		{
+			printf("%i, ", i);
+		}
+	} else
 	{
-
-		while (n < 98)
-
-			printf("%d, ", n++);
-
-		printf("%d\n", n);
-
+		for (i = n; i > 98; i--)
+		{
+			printf("%i, ", i);
+		}
 	}
-
+	printf("98\n");
 }
-
-
