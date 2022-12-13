0a3fdde8d8a2c53434f42cab9e8bd99ce5ea3a02
‘Studio-Prince’ <mosesice5@gmail.com>
2022-12-13 06:16:45 -0800

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,12 +7,12 @@ void print_alphabet_x10(void)
 {
 	char i;
 
-	for (i = 0; i < 9; i++)
+	for (i = 0; i < 10; i++)
 	{
-	for (i = 'a'; i <= 'z'; i++)
-	{
-		_putchar(i);
-	}
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
 		_putchar('\n');
 	}
 }
