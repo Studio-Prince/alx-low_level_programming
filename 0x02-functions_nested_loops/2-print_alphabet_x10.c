3e1716c92d9363c24c36fb94f22c306cade1d718
‘Studio-Prince’ <mosesice5@gmail.com>
2022-12-13 04:57:47 -0800

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*print_alphabet_x10 - prints 10 times the alphabet, in lowercase
+*
+*/
+void print_alphabet_x10(void)
+{
+
+	for (i = 0; i < 9; i++)
+	{
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+		_putchar('\n');
+	}
+}
