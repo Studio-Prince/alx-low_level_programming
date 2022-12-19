7921454dfc89bf204d40897867c5d7bd9a711196
‘Studio-Prince’ <mosesice5@gmail.com>
2022-12-19 05:39:01 -0800

    Don't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -12,22 +12,11 @@ int main(void)
 
 	time_t t;
 
+	srand(time(0));
 
+	int num = rand();
 
-	srand((unsigned int) time(&t));
+	printf("%i\n", num);
 
-	while (c < 2772)
-	{
-		r = rand() % 128;
-
-		if ((c + r) > 2772)
-
-			break;
-
-		c = c + r;
-
-		printf("%c", r);
-	}
-	printf("%c\n", (2772 - c));
 	return (0);
 }
