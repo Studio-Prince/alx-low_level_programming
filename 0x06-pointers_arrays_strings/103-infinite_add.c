988c3fd72365adbdf50a61840fbb8f159a3ede06
‘Studio-Prince’ <mosesice5@gmail.com>
2022-12-23 08:50:53 -0800

    It is the addition of strangeness to beauty that constitutes the romantic character in art

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,7 +1,5 @@
 #include "main.h"
-#include <stdio.h>
-/**
- * infinite_add - function that adds two numbers
+ /** infinite_add - function that adds two numbers
  * @n1: first number
  * @n2: second number
  * @r: buffer that the function will use to store the result
