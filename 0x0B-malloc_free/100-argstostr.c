a894a7ab4a0d17d8067c13d3306451bd07f8a64a
‘Studio-Prince’ <mosesice5@gmail.com>
2023-01-09 05:33:47 -0800

    It isn't the mountains ahead to climb that wear you out; it's the pebble in your shoe

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -7,7 +7,6 @@
   *Return: NULL if ac == 0 or av == null, Pointer to new string.
   *NULL on fail.
   */
-
 char *argstostr(int ac, char **av)
 {
 	int i, j, k, size;
@@ -15,54 +14,39 @@ char *argstostr(int ac, char **av)
 
 	size = 0;
 	k = 0;
-
 	if (ac == 0 || av == NULL)
 
 		return (NULL);
-
 	i = 0;
-
 	while (i < ac)
 	{
-
 		j = 0;
-
 		while (av[i][j])
 		{
-
 			size++;
 			j++;
 		}
-
 		size++;
 		i++;
 	}
-
 	arg = malloc((sizeof(char) * size) + 1);
-
 	if (arg == NULL)
-
 		return (NULL);
-
 	i = 0;
 
 	while (i < ac)
 	{
 		j = 0;
-
 		while (av[i][j])
 		{
 			arg[k] = av[i][j];
-
 			j++;
 			k++;
 		}
 		arg[k] = '\n';
-
 		k++;
 		i++;
 	}
 	arg[k] = '\0';
-
 	return (arg);
 }
